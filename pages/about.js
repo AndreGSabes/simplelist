4790bbc09eeca96500e2f6ebe94c989ff6eb3f8c
Andre Sabes <andresabes@outlook.com>
2021-04-15 23:23:36 -0300

    Courses 6-10

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,28 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sit modi
-        quae quidem dolor dolorem magnam, perspiciatis quo doloremque
-        exercitationem ea omnis dignissimos fuga rem, expedita, accusantium
-        illo. Illo, libero.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sit modi
-        quae quidem dolor dolorem magnam, perspiciatis quo doloremque
-        exercitationem ea omnis dignissimos fuga rem, expedita, accusantium
-        illo. Illo, libero.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"></meta>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sit modi
+          quae quidem dolor dolorem magnam, perspiciatis quo doloremque
+          exercitationem ea omnis dignissimos fuga rem, expedita, accusantium
+          illo. Illo, libero.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sit modi
+          quae quidem dolor dolorem magnam, perspiciatis quo doloremque
+          exercitationem ea omnis dignissimos fuga rem, expedita, accusantium
+          illo. Illo, libero.
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,29 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sit modi
-        quae quidem dolor dolorem magnam, perspiciatis quo doloremque
-        exercitationem ea omnis dignissimos fuga rem, expedita, accusantium
-        illo. Illo, libero.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sit modi
-        quae quidem dolor dolorem magnam, perspiciatis quo doloremque
-        exercitationem ea omnis dignissimos fuga rem, expedita, accusantium
-        illo. Illo, libero.
-      </p>
-      <Link href="/ninjas">
-        <a>See ninja listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"></meta>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sit modi
+          quae quidem dolor dolorem magnam, perspiciatis quo doloremque
+          exercitationem ea omnis dignissimos fuga rem, expedita, accusantium
+          illo. Illo, libero.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sit modi
+          quae quidem dolor dolorem magnam, perspiciatis quo doloremque
+          exercitationem ea omnis dignissimos fuga rem, expedita, accusantium
+          illo. Illo, libero.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See ninja listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
